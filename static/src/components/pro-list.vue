04c80070b02671856e5fc8c7936f050cec87bfe3
zhanghua <zhanghua8003@gmail.com>
2016-07-20 17:40:48 +0800

    页面调整

---
+++ static/src/components/pro-list.vue
@@ -0,0 +1,64 @@
+<style lang="less">
+	.pro-list-item{float: left; width: 50%; box-sizing: border-box; padding: 0 5/32rem 0 10/32rem; text-align: center; position: relative;
+		a{color: #333; text-decoration: none;}
+		&:nth-child(2n+1){padding: 0 10/32rem 0 5/32rem;}
+		img{height: 170/32rem;}
+		.zhekou_tag{position: absolute; top: -5/32rem; right: 10/32rem; width: 40/32rem; height: 40/32rem; border-radius: 60/32rem ;background-color: #fe5768; color: #fff; font-size: 12/32rem; play: flex; align-items:center; padding-top: 3/32rem;
+			span{display: block; font-size: 14/32rem;}
+		}
+		.price{ font-size: 14/32rem; font-weight: bold;}
+	}
+</style>
+<template>
+	<li class="pro-list-item">
+		<a href="#">
+			<strong class="zhekou_tag"><span>62%</span>OFF</strong>
+			<img src="http://gloimg.sammydress.com/S/pdm-product-pic/Clothing/2016/05/18/goods-img/1464201782954321887.jpg" alt="">
+			<p class="price">
+				<span>10.72</span>
+			</p>
+		</a>
+	</li>
+	<li class="pro-list-item">
+		<a href="#">
+			<strong class="zhekou_tag"><span>62%</span>OFF</strong>
+			<img src="http://gloimg.sammydress.com/S/pdm-product-pic/Clothing/2016/05/18/goods-img/1464201782954321887.jpg" alt="">
+			<p class="price">
+				<span>10.72</span>
+			</p>
+		</a>
+	</li>
+	<li class="pro-list-item">
+		<a href="#">
+			<strong class="zhekou_tag"><span>62%</span>OFF</strong>
+			<img src="http://gloimg.sammydress.com/S/pdm-product-pic/Clothing/2016/05/18/goods-img/1464201782954321887.jpg" alt="">
+			<p class="price">
+				<span>10.72</span>
+			</p>
+		</a>
+	</li>
+	<li class="pro-list-item">
+		<a href="#">
+			<strong class="zhekou_tag"><span>62%</span>OFF</strong>
+			<img src="http://gloimg.sammydress.com/S/pdm-product-pic/Clothing/2016/05/18/goods-img/1464201782954321887.jpg" alt="">
+			<p class="price">
+				<span>10.72</span>
+			</p>
+		</a>
+	</li>
+	<li class="pro-list-item">
+		<a href="#">
+			<strong class="zhekou_tag"><span>62%</span>OFF</strong>
+			<img src="http://gloimg.sammydress.com/S/pdm-product-pic/Clothing/2016/05/18/goods-img/1464201782954321887.jpg" alt="">
+			<p class="price">
+				<span>10.72</span>
+			</p>
+		</a>
+	</li>
+</template>
+
+<script type="text/javascript">
+	export default {
+
+	}
+</script>
